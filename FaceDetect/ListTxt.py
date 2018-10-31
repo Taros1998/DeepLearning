f481e99d5bcd7fc1f9462453c579a6b083bddb37
Taros1998 <0524073@nkust.edu.tw>
2018-11-01 06:21:08 +0800

    181101

--- FaceDetect/ListTxt.py
+++ FaceDetect/ListTxt.py
@@ -1,2 +1,3 @@
-foo = open("train.txt", "w")
-bar = open("val.txt", "w")
+version https://git-lfs.github.com/spec/v1
+oid sha256:7b952f750ec92f6e745636f6436276cd10dabe45c679fc92d0f48282b10f4ceb
+size 56
--- FaceDetect/trainRename.py
+++ FaceDetect/trainRename.py
@@ -1,17 +1,3 @@
-import os
-foo = open("train.txt", "w")
-
-path1 = r"C:\Users\taros\FaceDetect\train\acne"
-path2 = r"C:\Users\taros\FaceDetect\train\norm"
-i = 1
-j = 1
-for file in os.listdir(path1):
-    newname='acne_%d.jpeg'%i
-    os.rename(os.path.join(path1,file),os.path.join(path1,newname))
-    foo.write("/acne/"+newname+" 0\n")
-    i += 1
-for file in os.listdir(path2):
-    newname='norm_%d.jpeg'%j
-    os.rename(os.path.join(path2,file),os.path.join(path2,newname))
-    foo.write("/norm/"+newname+" 1\n")
-    j += 1
+version https://git-lfs.github.com/spec/v1
+oid sha256:860f4b9908e00a47d91f931d0017486c955a8934a829b9b12b238af53156e9da
+size 504
