424b663dd41f5e61131c59a56a1fe40003e443bb
Taros1998 <0524073@nkust.edu.tw>
2018-11-01 10:47:43 +0800

    181101

---
+++ FaceDetect/FaceDetect.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:553418b720877b86eb857977f3aa8eecc6ec55b72b89cce834f7e4bac375d212
+size 1476
---
+++ FaceDetect/npConvert.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb94cda3d1981499fdef8fa2cac6d61151ab721aa1bfa2d1db4f52895aa893ec
+size 387
